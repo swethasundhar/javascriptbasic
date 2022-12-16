5cdd0ff7fe223fd199d32bba6a6098a7b4897656
swethasundhar <75666721+swethasundhar@users.noreply.github.com>
2022-12-16 19:45:00 +0530

    comit-5

---
+++ loops.js
@@ -0,0 +1,12 @@
+//For loop - known no.of iterations
+for (let i = 0; i < 100; i++) {
+    console.log(i);
+}
+
+//while loop- unknown no.of iterations
+let num = 0987654321
+
+while (num < 0) {
+    console.log(num % 10);
+    num - parseInt(num / 10)
+}
